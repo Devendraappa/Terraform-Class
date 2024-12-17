62093dbf6fb4134131529fd122b4f994167b4c6e
Devendraappa <devendraappa.kune@gmail.com>
2024-12-17 12:14:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket  = "training-bucket22" # Replace with your bucket name
+    key     = "state/terraform.tfstate"     # Replace with the desired path in the bucket
+    region  = "ap-south-1"                  # Replace with your AWS region
+    encrypt = true                          # Encrypt the state file
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
 
+# Create an S3 bucket for the Terraform state
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "your-terraform-state-bucket" # Replace with a unique bucket name
+  acl    = "private"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+# VPC resource
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -11,6 +31,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Subnet resource
 resource "aws_subnet" "subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -20,6 +41,7 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+# Internet Gateway resource
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
