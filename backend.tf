46fcc38f5c84806fc4a4cc7545b3137b36f948fa
Devendraappa <devendraappa.kune@gmail.com>
2024-12-17 13:07:53 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "training-test678"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
