75ae4434d6a935ae6f405ca32b98bb2c3abc541d
Devendraappa <devendraappa.kune@gmail.com>
2024-12-17 13:20:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Create an S3 bucket for the Terraform state
-resource "aws_s3_bucket" "state_bucket" {
-  bucket = "training-test456" # Replace with a unique bucket name
-  acl    = "private"
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
 # VPC resource
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
@@ -37,5 +27,26 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "main-igw"
+  }
+}
+
+# Create an S3 bucket for the Terraform state
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "training-test6785" 
+  acl    = "private"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+
+#ec2-creation
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
   }
 }
